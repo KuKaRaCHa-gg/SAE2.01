8571e7a032a9942045b5262811b6d35e512d779a
MattisGoom64 <schaefermattis64@gmail.com>
2024-05-29 08:44:05 +0200

    Ajout des page

---
+++ src/main/java/com/example/sae_201/accueil.java
@@ -0,0 +1,119 @@
+package com.example.sae_201;
+
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+
+public class accueil {
+
+    @FXML
+    private HBox ajoutRecLabel;
+
+    @FXML
+    private HBox alphaLabel;
+
+    @FXML
+    private ImageView gameImage1;
+
+    @FXML
+    private ImageView gameImage10;
+
+    @FXML
+    private ImageView gameImage2;
+
+    @FXML
+    private ImageView gameImage3;
+
+    @FXML
+    private ImageView gameImage4;
+
+    @FXML
+    private ImageView gameImage5;
+
+    @FXML
+    private ImageView gameImage6;
+
+    @FXML
+    private ImageView gameImage7;
+
+    @FXML
+    private ImageView gameImage8;
+
+    @FXML
+    private ImageView gameImage9;
+
+    @FXML
+    private Label gameLabel1;
+
+    @FXML
+    private Label gameLabel10;
+
+    @FXML
+    private Label gameLabel2;
+
+    @FXML
+    private Label gameLabel3;
+
+    @FXML
+    private Label gameLabel4;
+
+    @FXML
+    private Label gameLabel5;
+
+    @FXML
+    private Label gameLabel6;
+
+    @FXML
+    private Label gameLabel7;
+
+    @FXML
+    private Label gameLabel8;
+
+    @FXML
+    private Label gameLabel9;
+
+    @FXML
+    private ImageView image;
+
+
+    @FXML
+    private ImageView imageTendance1;
+
+    @FXML
+    private ImageView imageTendance2;
+
+    @FXML
+    private ImageView imageTendance3;
+
+    @FXML
+    private ImageView imageTendance4;
+
+    @FXML
+    private ImageView imageTendance5;
+    @FXML
+    private VBox textBienvenu;
+
+
+
+
+    public void onTestClicked(javafx.event.ActionEvent event) {
+        ajoutRecLabel.setOpacity(1);
+        gameLabel1.setOpacity(1);
+        gameLabel2.setOpacity(1);
+        gameLabel3.setOpacity(1);
+        gameLabel4.setOpacity(1);
+        gameLabel5.setOpacity(1);
+        textBienvenu.setOpacity(0);
+
+    }
+
+
+
+
+
+}
