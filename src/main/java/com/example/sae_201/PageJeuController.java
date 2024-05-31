13daf1cb75acd4f663214b027659d2f028579e28
Daniil MINEVICH <daniilpub@gmail.com>
2024-05-31 11:58:10 +0200

    Creation PageJeuController

--- src/main/java/com/example/sae_201/PageJeuController.java
+++ src/main/java/com/example/sae_201/PageJeuController.java
@@ -1,6 +1,28 @@
 package com.example.sae_201;
 
-public class PageJeuController {
+import javafx.fxml.FXML;
 
+public class PageJeuController {
+    @FXML
+    private void handleMesJeuxButtonAction(ActionEvent event) throws IOException {
+        loadScene("MainScreen.fxml");
+    }
+
+    @FXML
+    private void handleTagsButtonAction(ActionEvent event) throws IOException {
+        loadScene("TagsScreen.fxml");
+    }
+
+    @FXML
+    private void handleCreerTagButtonAction(ActionEvent event) throws IOException {
+        loadScene("CreateTagScreen.fxml");
+    }
+
+    private void loadScene(String fxml) throws IOException {
+        Stage stage = (Stage) mesJeuxButton.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
 
 }
