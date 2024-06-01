47f8666814bed27c387821b018bb8b08576379c9
MattisGoom64 <schaefermattis64@gmail.com>
2024-06-01 10:22:58 +0200

    Update HelloApplication.java

--- src/main/java/com/example/sae_201/HelloApplication.java
+++ src/main/java/com/example/sae_201/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("accueil.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gestionnaire de jeu");
         stage.setScene(scene);
         stage.show();
         // test
